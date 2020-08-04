da310d11b4c06cd5aa47d7d0fab8035349e563a9
guillenjoshua <guillenjoshua@gmail.com>
2020-08-04 01:28:37 -0500

    All New Code

--- script.js
+++ script.js
@@ -1,95 +1,39 @@
 
 
-//Handle This Prompt
-// var handleThis = confirm("Are you ready for This?");
-
-//  if (handleThis){
-//          alert("Lets get this party started!!!");
-//          alert("CLICK START WHEN READY!");
-//     }else{
-//         alert("Alrighty then, maybe next time!");
-//  }
-
- //Button to Start
-
- var heading=document.createElement("h1")
- heading.textContent = "Let us now begin the Quiz!"
- document.getElementById("intro").appendChild(heading)
-
-// Start button
- var button=document.createElement("button")
-    button.textContent = "Start"
-    document.querySelector("#start").appendChild(button)
 
 
-  //Answer Buttons
-  var button=document.createElement("button")
-  button.textContent = "A"
-  document.querySelector(".button1").appendChild(button)
+//Questions
 
-  var button=document.createElement("button")
-  button.textContent = "B"
-  document.querySelector(".button2").appendChild(button)
+var questions = [
+  {
+    q: "Who was the second President of the United States?",
+    choices:["John Adams", "Abe Lincoln", "GW", "Eisenhower"],
+    answer:0
+}, 
 
-  var button=document.createElement("button")
-  button.textContent = "C"
-  document.querySelector(".button3").appendChild(button)
-
-  var button=document.createElement("button")
-  button.textContent = "D"
-  document.querySelector(".button4").appendChild(button)
-
- 
+{
+  q: "Who is the wielder of the Master Sword?",
+choices:["Mario", "Samus", "Zelda", "Link"],
+answer:2
+}, 
 
 
-  //Begin Quiz
+{q: "What is the capital of Texas?",
+choices:["Houston", "Dallas", "Moscow", "Austin"],
+answer:3
+}, 
 
-  var startbutton = document.querySelector("#start");
-  var answerEl = document.querySelector("#questioncontainer");
-  var heading = document.querySelector("#intro");
+{
+  q: "Who would win in a Battle Royale?",
+choices:["Superman", "Ant-Man", "Batman", "Thanos"],
+answer:2
+},
 
-//Insert Questions
- var questionEl = document.querySelector("#questions");
- var buttonEl = document.querySelector(".buttons");
-
- let randomQ, questionStart;
-
-  startbutton.addEventListener("click",beginQuiz);
-
-  function beginQuiz() {
-     heading.classList.add("hide")
-     startbutton.classList.add("hide")
-     randomQ = questions.sort(() => Math.random() - .5)
-     questionStart = 0
-     answerEl.classList.remove("hide")
-     questionNext()
-        }
-       
-
-
-//functions to run questions
-
-function questionNext() {
- runQuestions(randomQ[questionStart])
-}
-       
-function runQuestions(question){
-       questionEl.innerText = question.question
-       
-}
-
-//Funtions to answer questions
-
-//Question Sets
-
-let questions = [
- {
-  question: "Hello?",
-  answer: [
-    {text: "Batman", correct: true},
-    {text: "Superman", correct: false}
-  ]
- }
+{
+  q: "Who was the first man on the moon?",
+choices:["Aldrin", "Armstrong", "Putin", "Trump"],
+answer:2
+},
 ]
 
 
@@ -98,20 +42,154 @@ let questions = [
 
 
 
- /*$(".btn").append(button);
 
-var secondsLeft = 0; 
+ //Button to Start
 
- $(".btn").on("click", function setTime(){
+var heading=document.createElement("h2")
+heading.textContent = "Let us now begin the Quiz!"
+document.getElementById("intro").appendChild(heading)
 
-    var timerInterval = setInterval(function() {
-        secondsLeft--;
-        button.text = secondsLeft;
+// Start button
+//  var button=document.createElement("button")
+//     button.textContent = "Start"
+//     document.querySelector("#start").appendChild(button)
+
+
+//Setup
+var afterClick = document.querySelector(".afterClick")
+var beforeClick = document.querySelector('.beforeClick')
+var startButton = document.querySelector(".start");
+var homeBox = document.querySelector(".begin");
+var questionContainer = document.querySelector(".question-container")
+
+var questionNumber = document.querySelector(".question-no");
+var questionText = document.querySelector(".question-text");
+var choiceContainer = document.querySelector(".choice-container");
+
+var questionCounter = 0;
+var currentQuestion;
+var questionsLeft = [];
+var answersLeft = [];
+
+
+// start button remove
+
+      function startQuiz() {
+         afterClick.classList.add("hide")
+        beforeClick.classList.remove("hide")
+        inputQuestions();
+     }
+
+
+
+
+
+
+//Setup for Questions - Send to HTML
+function inputQuestions(){
+  var totalQuestions = questions.length;
+  for (var i = 0; i<totalQuestions; i++){
+    questionsLeft.push(questions[i])
+  }
+}
+
+function nextQuestion(){
+  questionNumber.innerHTML = "Question " + (questionCounter + 1) + " of " + questions.length;
+
+  var questionIndex = questionsLeft[Math.floor(Math.random() * questionsLeft.length)]
+  currentQuestion = questionIndex;
+  questionText.innerHTML = currentQuestion.q;
+
+  var index1 = questionsLeft.indexOf(questionIndex);
+questionsLeft.splice(index1,1);
+
+  
+  // Setup for Answers
+  
+  var optionsLength = currentQuestion.choices.length
+
+  for (var i = 0; i<optionsLength; i++ ){
+      answersLeft.push(i)
+  }
+
+
+
+
+
+//Send Answers to HTML
+  for (var i = 0; i<optionsLength; i++ ){
+
+      var answerIndex = answersLeft[Math.floor(Math.random()*answersLeft.length)];
+      
+      var index2 = answersLeft.indexOf(answerIndex);
+      answersLeft.splice(index2,1);
+  
+    var option = document.createElement("div");
+    option.innerHTML = currentQuestion.choices[answerIndex];
+    option.id = i;
+    option.className = "choice";
+    choiceContainer.appendChild(option)
+
+    //Correct Answer Choice
+    option.setAttribute("onclick", "getResult(this)");
+
     
-        if(secondsLeft === 0) {
-          clearInterval(timerInterval);
-          sendMessage();
-        }
+  }
+  questionCounter++
+  
+}
 
- }, 1000);
- }*/
+
+function resultsNew (element){
+  var id = parseInt(element.id);
+    if (id === currentQuestion.answer){
+      element.classList.add("Correct!")
+    } else {
+     element.classList.add("Incorrect!")
+    }
+
+
+}
+
+//End of Quiz Function
+function next(){
+    if(questionCounter === questions.length){
+      console.log("Quiz Over")
+    } else {
+      nextQuestion();
+    }
+}
+
+
+
+
+window.onload = function(){
+
+  inputQuestions();
+  nextQuestion();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
